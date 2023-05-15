02ca691639efbe420e862f4ef777fda5277f89ab
yoyocortex <22plepelic22@gmail.com>
2023-05-15 13:47:19 +0200

    Added images to project

--- MetaCarsWebAssembly/Program.cs
+++ MetaCarsWebAssembly/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MetaCarsWebAssembly;
 using Microsoft.Extensions.DependencyInjection;
+using Google.Cloud.Firestore;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
