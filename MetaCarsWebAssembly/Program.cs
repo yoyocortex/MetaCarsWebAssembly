c1c4bd851db66964837b65ae4bbe1813fa9f1ea0
yoyocortex <22plepelic22@gmail.com>
2023-05-15 18:27:22 +0200

    Pictures to be classified by user

--- MetaCarsWebAssembly/Program.cs
+++ MetaCarsWebAssembly/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MetaCarsWebAssembly;
 using Microsoft.Extensions.DependencyInjection;
-using Google.Cloud.Firestore;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -13,4 +13,8 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
 await builder.Build().RunAsync();
