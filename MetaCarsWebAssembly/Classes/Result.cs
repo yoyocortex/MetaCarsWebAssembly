d0edd094a8fae1981d73775c34650c4166860f04
yoyocortex <22plepelic22@gmail.com>
2023-05-17 15:46:14 +0200

    First and last page updated

---
+++ MetaCarsWebAssembly/Classes/Result.cs
@@ -0,0 +1,9 @@
+namespace MetaCarsWebAssembly.Classes {
+    public class Result {
+        public string AiScore {get;set;}
+
+        public string UserScore {get;set;}
+        
+        public string Username {get;set;}
+    }
+}
