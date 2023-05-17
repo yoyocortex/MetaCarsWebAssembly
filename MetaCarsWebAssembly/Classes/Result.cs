6aace023505779c3e80796c1f95bf2497bb9082e
yoyocortex <22plepelic22@gmail.com>
2023-05-17 17:01:29 +0200

    Final commit

--- MetaCarsWebAssembly/Classes/Result.cs
+++
@@ -1,9 +0,0 @@
-namespace MetaCarsWebAssembly.Classes {
-    public class Result {
-        public string AiScore {get;set;}
-
-        public string UserScore {get;set;}
-        
-        public string Username {get;set;}
-    }
-}
